7503b68fd7f99a6b4ce684868ad5c3473151040d
Zenbeggar <zenbeggar@gmail.com>
2026-04-28 23:44:10 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,36 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+const CACHE_NAME = 'jugger-steine-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app-v3.js',
+  '/StoneSmash.wav',
+  '/favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+});
+``
